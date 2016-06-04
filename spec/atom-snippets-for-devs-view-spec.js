92acfdcc3e923611c856f0d6203a5b957898050e
marcelohmdias <marcelohmdias@gmail.com>
2016-06-04 18:04:14 -0300

    first commit

---
+++ spec/atom-snippets-for-devs-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomSnippetsForDevsView from '../lib/atom-snippets-for-devs-view';
+
+describe('AtomSnippetsForDevsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
